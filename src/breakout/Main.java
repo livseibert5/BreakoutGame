0c387445b418f49271b18835f6e0b88449f0fe01
Livia <las120@duke.edu>
2021-01-23 14:48:02 -0500

    scaffolding for main file

--- src/breakout/Main.java
+++ src/breakout/Main.java
@@ -1,13 +1,38 @@
 package breakout;
 
-/**
- * Feel free to completely change this code or delete it entirely. 
- */
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Paint;
+
+public class Main extends Application {
+
+  private GameController controller;
+
   /**
-   * Start of the program.
+   * Initialize what will be displayed and how it will be updated.
+   */
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    // TODO: Show menu screen and instructions.
+    // TODO: Launch step function.
+  }
+
+  // Change properties of shapes in small ways to animate them over time.
+  private void step (double elapsedTime) {
+    // TODO: Call game controller to set the level up and commence game play.
+  }
+
+  // What to do each time a key is pressed.
+  private void handleKeyInput(KeyCode code) {
+
+  }
+
+  /**
+   * Start the program.
    */
   public static void main(String[] args) {
-    System.out.println("Hello world");
+    launch(args);
   }
 }
